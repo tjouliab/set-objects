4a57523e3880906b30c509c835e20c5ab602d04d
Tim <tim.jouliab@icloud.com>
2025-01-26 17:57:34 +0100

    implement more tests for SetObjects constructor

--- src/set-objects.spec.ts
+++ src/set-objects.spec.ts
@@ -12,168 +12,433 @@ interface MockObjectIdArray<T> {
 interface MockObjectIdObject<T extends Object> {
   id: T;
 }
+interface MockObjectWithData<T> {
+  id: number;
+  data: T;
+}
 
 describe("constructor", () => {
-  it("should handle an empty list", () => {
-    const objects: never[] = [];
-    const expected: never[] = [];
-    const result: never[] = [...new SetObjects(objects)];
-    expect(result).toEqual(expected);
+  describe("without key", () => {
+    it("should handle an empty list", () => {
+      const objects: never[] = [];
+      const expected: never[] = [];
+      const result: never[] = [...new SetObjects(objects)];
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with id of type number", () => {
+      const objects: MockObjectIdNumber[] = [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+      ];
+
+      const expected: MockObjectIdNumber[] = [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+      ];
+
+      const result: MockObjectIdNumber[] = [...new SetObjects(objects)];
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with id of type string", () => {
+      const objects: MockObjectIdString[] = [
+        {
+          id: "1",
+        },
+        {
+          id: "2",
+        },
+        {
+          id: "3",
+        },
+        {
+          id: "1",
+        },
+        {
+          id: "2",
+        },
+      ];
+
+      const expected: MockObjectIdString[] = [
+        {
+          id: "1",
+        },
+        {
+          id: "2",
+        },
+        {
+          id: "3",
+        },
+      ];
+
+      const result: MockObjectIdString[] = [...new SetObjects(objects)];
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with id of type array", () => {
+      const objects: MockObjectIdArray<string>[] = [
+        {
+          id: ["1"],
+        },
+        {
+          id: ["2"],
+        },
+        {
+          id: ["3"],
+        },
+        {
+          id: ["1"],
+        },
+        {
+          id: ["1", "1"],
+        },
+        {
+          id: ["2"],
+        },
+      ];
+
+      const expected: MockObjectIdArray<string>[] = [
+        {
+          id: ["1"],
+        },
+        {
+          id: ["2"],
+        },
+        {
+          id: ["3"],
+        },
+        {
+          id: ["1", "1"],
+        },
+      ];
+
+      const result: MockObjectIdArray<string>[] = [...new SetObjects(objects)];
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with id of type object", () => {
+      const objects: MockObjectIdObject<{ value: string }>[] = [
+        {
+          id: { value: "1" },
+        },
+        {
+          id: { value: "2" },
+        },
+        {
+          id: { value: "3" },
+        },
+        {
+          id: { value: "1" },
+        },
+        {
+          id: { value: "11" },
+        },
+        {
+          id: { value: "2" },
+        },
+      ];
+
+      const expected: MockObjectIdObject<{ value: string }>[] = [
+        {
+          id: { value: "1" },
+        },
+        {
+          id: { value: "2" },
+        },
+        {
+          id: { value: "3" },
+        },
+        {
+          id: { value: "11" },
+        },
+      ];
+
+      const result: MockObjectIdObject<{ value: string }>[] = [
+        ...new SetObjects(objects),
+      ];
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with data", () => {
+      const objects: MockObjectWithData<string>[] = [
+        {
+          id: 1,
+          data: 'data',
+        },
+        {
+          id: 2,
+          data: 'data',
+        },
+        {
+          id: 3,
+          data: 'data',
+        },
+        {
+          id: 1,
+          data: 'data',
+        },
+        {
+          id: 2,
+          data: 'data',
+        },
+      ];
+
+      const expected: MockObjectWithData<string>[] = [
+        {
+          id: 1,
+          data: 'data',
+        },
+        {
+          id: 2,
+          data: 'data',
+        },
+        {
+          id: 3,
+          data: 'data',
+        },
+      ];
+
+      const result: MockObjectWithData<string>[] = [...new SetObjects(objects)];
+
+      expect(result).toEqual(expected);
+    });
   });
 
-  it("should handle a list of objects with id of type number", () => {
-    const objects: MockObjectIdNumber[] = [
-      {
-        id: 1,
-      },
-      {
-        id: 2,
-      },
-      {
-        id: 3,
-      },
-      {
-        id: 1,
-      },
-      {
-        id: 2,
-      },
-    ];
+  describe("with key", () => {
+    it("should handle an empty list", () => {
+      const objects: never[] = [];
+      const expected: never[] = [];
+      const result: never[] = [...new SetObjects(objects, "key")];
+      expect(result).toEqual(expected);
+    });
 
-    const expected: MockObjectIdNumber[] = [
-      {
-        id: 1,
-      },
-      {
-        id: 2,
-      },
-      {
-        id: 3,
-      },
-    ];
+    it("should handle a list of objects with id of type number", () => {
+      const objects: MockObjectIdNumber[] = [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+      ];
 
-    const result: MockObjectIdNumber[] = [...new SetObjects(objects)];
+      const expected: MockObjectIdNumber[] = [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+      ];
 
-    expect(result).toEqual(expected);
-  });
+      const result: MockObjectIdNumber[] = [...new SetObjects(objects, "id")];
 
-  it("should handle a list of objects with id of type string", () => {
-    const objects: MockObjectIdString[] = [
-      {
-        id: '1',
-      },
-      {
-        id: '2',
-      },
-      {
-        id: '3',
-      },
-      {
-        id: '1',
-      },
-      {
-        id: '2',
-      },
-    ];
+      expect(result).toEqual(expected);
+    });
 
-    const expected: MockObjectIdString[] = [
-      {
-        id: '1',
-      },
-      {
-        id: '2',
-      },
-      {
-        id: '3',
-      },
-    ];
+    it("should handle a list of objects with id of type string", () => {
+      const objects: MockObjectIdString[] = [
+        {
+          id: "1",
+        },
+        {
+          id: "2",
+        },
+        {
+          id: "3",
+        },
+        {
+          id: "1",
+        },
+        {
+          id: "2",
+        },
+      ];
 
-    const result: MockObjectIdString[] = [...new SetObjects(objects)];
-    
-    expect(result).toEqual(expected);
-  });
+      const expected: MockObjectIdString[] = [
+        {
+          id: "1",
+        },
+        {
+          id: "2",
+        },
+        {
+          id: "3",
+        },
+      ];
 
-  it("should handle a list of objects with id of type array", () => {
-    const objects: MockObjectIdArray<string>[] = [
-      {
-        id: ['1'],
-      },
-      {
-        id: ['2'],
-      },
-      {
-        id: ['3'],
-      },
-      {
-        id: ['1'],
-      },
-      {
-        id: ['1', '1'],
-      },
-      {
-        id: ['2'],
-      },
-    ];
+      const result: MockObjectIdString[] = [...new SetObjects(objects, "id")];
 
-    const expected: MockObjectIdArray<string>[] = [
-      {
-        id: ['1'],
-      },
-      {
-        id: ['2'],
-      },
-      {
-        id: ['3'],
-      },
-      {
-        id: ['1', '1'],
-      },
-    ];
+      expect(result).toEqual(expected);
+    });
 
-    const result: MockObjectIdArray<string>[] = [...new SetObjects(objects)];
-    
-    expect(result).toEqual(expected);
-  });
+    it("should handle a list of objects with id of type array", () => {
+      const objects: MockObjectIdArray<string>[] = [
+        {
+          id: ["1"],
+        },
+        {
+          id: ["2"],
+        },
+        {
+          id: ["3"],
+        },
+        {
+          id: ["1"],
+        },
+        {
+          id: ["1", "1"],
+        },
+        {
+          id: ["2"],
+        },
+      ];
 
-  it("should handle a list of objects with id of type object", () => {
-    const objects: MockObjectIdObject<{value: string}>[] = [
-      {
-        id: {value: '1'},
-      },
-      {
-        id: {value: '2'},
-      },
-      {
-        id: {value: '3'},
-      },
-      {
-        id: {value: '1'},
-      },
-      {
-        id: {value: '11'},
-      },
-      {
-        id: {value: '2'},
-      },
-    ];
+      const expected: MockObjectIdArray<string>[] = [
+        {
+          id: ["1"],
+        },
+        {
+          id: ["2"],
+        },
+        {
+          id: ["3"],
+        },
+        {
+          id: ["1", "1"],
+        },
+      ];
 
-    const expected: MockObjectIdObject<{value: string}>[] = [
-      {
-        id: {value: '1'},
-      },
-      {
-        id: {value: '2'},
-      },
-      {
-        id: {value: '3'},
-      },
-      {
-        id: {value: '11'},
-      },
-    ];
+      const result: MockObjectIdArray<string>[] = [
+        ...new SetObjects(objects, "id"),
+      ];
 
-    const result: MockObjectIdObject<{value: string}>[] = [...new SetObjects(objects)];
-    
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with id of type object", () => {
+      const objects: MockObjectIdObject<{ value: string }>[] = [
+        {
+          id: { value: "1" },
+        },
+        {
+          id: { value: "2" },
+        },
+        {
+          id: { value: "3" },
+        },
+        {
+          id: { value: "1" },
+        },
+        {
+          id: { value: "11" },
+        },
+        {
+          id: { value: "2" },
+        },
+      ];
+
+      const expected: MockObjectIdObject<{ value: string }>[] = [
+        {
+          id: { value: "1" },
+        },
+        {
+          id: { value: "2" },
+        },
+        {
+          id: { value: "3" },
+        },
+        {
+          id: { value: "11" },
+        },
+      ];
+
+      const result: MockObjectIdObject<{ value: string }>[] = [
+        ...new SetObjects(objects, "id"),
+      ];
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should handle a list of objects with data", () => {
+      const objects: MockObjectWithData<string>[] = [
+        {
+          id: 1,
+          data: 'data',
+        },
+        {
+          id: 2,
+          data: 'data',
+        },
+        {
+          id: 3,
+          data: 'data',
+        },
+        {
+          id: 1,
+          data: 'data',
+        },
+        {
+          id: 2,
+          data: 'data',
+        },
+      ];
+
+      const expected: MockObjectWithData<string>[] = [
+        {
+          id: 1,
+          data: 'data',
+        },
+        {
+          id: 2,
+          data: 'data',
+        },
+        {
+          id: 3,
+          data: 'data',
+        },
+      ];
+
+      const result: MockObjectWithData<string>[] = [...new SetObjects(objects, 'id')];
+
+      expect(result).toEqual(expected);
+    });
   });
 });
