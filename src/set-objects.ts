6a47e4535855ac24c107b8775221370714abc6dd
Tim <tim.jouliab@icloud.com>
2025-01-26 23:25:32 +0100

    implement a hashFn and add comments

--- src/set-objects.ts
+++ src/set-objects.ts
@@ -1,43 +1,64 @@
-export class SetObjects<T> {
-  private readonly objectsMap: Map<string, T> = new Map();
+/**
+ * A class that manages a set of objects with unique identifiers.
+ *
+ * @template T - The type of objects to be stored in the set.
+ * @template U - The type of the unique identifier for the objects, which can be either a string or a number.
+ */
+export class SetObjects<T extends object, U extends string | number> {
+  /**
+   * A function that generates a unique identifier for an object.
+   */
+  private readonly hashFn: (obj: T) => U;
 
-  constructor(objects: T[], key?: keyof T) {
-    for (const object of objects) {
-      this.add(object, key);
+  /**
+   * A map that stores the objects with their unique identifiers.
+   */
+  private readonly objectsMap: Map<U, T> = new Map();
+
+  /**
+   * Creates an instance of SetObjects.
+   *
+   * @param objects - An array of objects to be initially added to the set.
+   * @param fn - A function that generates a unique identifier for an object.
+   */
+  constructor(objects: T[], fn: (obj: T) => U) {
+    this.hashFn = fn;
+    for (const obj of objects) {
+      this.add(obj);
     }
   }
 
-  add(object: T, key?: keyof T): this {
-    const keyString: string = this.getKeyString(object, key);
-    if (!this.hasKeyString(keyString)) {
-      this.objectsMap.set(keyString, object);
-    }
-    return this;
+  /**
+   * Adds an object to the set.
+   *
+   * @param obj - The object to be added.
+   */
+  add(obj: T): void {
+    this.objectsMap.set(this.hashFn(obj), obj);
   }
 
-  has(object: T, key?: keyof T): boolean {
-    const keyString: string = this.getKeyString(object, key);
-    return this.hasKeyString(keyString);
+  /**
+   * Checks if an object is in the set.
+   *
+   * @param obj - The object to be checked.
+   * @returns `true` if the object is in the set, otherwise `false`.
+   */
+  has(obj: T): boolean {
+    return this.objectsMap.has(this.hashFn(obj));
   }
 
-  private getKeyString(object: T, key?: keyof T): string {
-    let keyString: string;
-    if (key == null) {
-      // Fallback if key is not provided
-      keyString = JSON.stringify(object);
-    } else if (key instanceof String) {
-      keyString = key as string;
-    } else {
-      // Should handle any type of key provided
-      keyString = JSON.stringify(object[key]);
-    }
-    return keyString;
+  /**
+   * Deletes an object from the set.
+   *
+   * @param obj - The object to be deleted.
+   */
+  delete(obj: T): void {
+    this.objectsMap.delete(this.hashFn(obj));
   }
 
-  private hasKeyString(keyString: string): boolean {
-    return this.objectsMap.has(keyString);
-  }
-
+  /**
+   * Make the collection iterable (for ... of).
+   */
   [Symbol.iterator](): IterableIterator<T> {
     return this.objectsMap.values();
   }
